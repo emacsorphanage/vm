05879259f0db73d56515ef537b87e631f6468c96
Robert Widhopf <hack@robf.de>
2005-11-11 01:37:00 +0000

    Added missing includes to stdlib.h to *.c Avoids compiler warnings.

--- base64-encode.c
+++ base64-encode.c
@@ -6,6 +6,7 @@
  * UNIX's newline convention is used, i.e. one ASCII control-j (10 decimal).
  */
 
+#include <stdlib.h>
 #include <stdio.h>
 
 #ifdef _WIN32
@@ -24,52 +25,52 @@ unsigned char alphabet[64] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwx
 int
 main()
 {
-    int cols, bits, c, char_count;
+	int cols, bits, c, char_count;
 
 #ifdef WIN32
-    _setmode( _fileno(stdin), _O_BINARY);
+	_setmode( _fileno(stdin), _O_BINARY);
 #endif
 
-    char_count = 0;
-    bits = 0;
-    cols = 0;
-    while ((c = getchar()) != EOF) {
+	char_count = 0;
+	bits = 0;
+	cols = 0;
+	while ((c = getchar()) != EOF) {
 	if (c > 255) {
-	    fprintf(stderr, "encountered char > 255 (decimal %d)", c);
-	    exit(1);
+		fprintf(stderr, "encountered char > 255 (decimal %d)", c);
+		exit(1);
 	}
 	bits += c;
 	char_count++;
 	if (char_count == 3) {
-	    putchar(alphabet[bits >> 18]);
-	    putchar(alphabet[(bits >> 12) & 0x3f]);
-	    putchar(alphabet[(bits >> 6) & 0x3f]);
-	    putchar(alphabet[bits & 0x3f]);
-	    cols += 4;
-	    if (cols == 72) {
+		putchar(alphabet[bits >> 18]);
+		putchar(alphabet[(bits >> 12) & 0x3f]);
+		putchar(alphabet[(bits >> 6) & 0x3f]);
+		putchar(alphabet[bits & 0x3f]);
+		cols += 4;
+		if (cols == 72) {
 		putchar('\n');
 		cols = 0;
-	    }
-	    bits = 0;
-	    char_count = 0;
+		}
+		bits = 0;
+		char_count = 0;
 	} else {
-	    bits <<= 8;
+		bits <<= 8;
 	}
-    }
-    if (char_count != 0) {
+	}
+	if (char_count != 0) {
 	bits <<= 16 - (8 * char_count);
 	putchar(alphabet[bits >> 18]);
 	putchar(alphabet[(bits >> 12) & 0x3f]);
 	if (char_count == 1) {
-	    putchar('=');
-	    putchar('=');
+		putchar('=');
+		putchar('=');
 	} else {
-	    putchar(alphabet[(bits >> 6) & 0x3f]);
-	    putchar('=');
+		putchar(alphabet[(bits >> 6) & 0x3f]);
+		putchar('=');
 	}
 	if (cols > 0)
 	  putchar('\n');
-    }
+	}
 
-    exit(0);
+	exit(0);
 }
