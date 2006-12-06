9772c37091ab8e4ffb3260ae8376109d22630ee6
Robert Widhopf-Fenk <hack@robf.de>
2006-12-06 21:39:12 +0100

    Bugfix: do not fail on replying to messages without a subject
    Thanks to Julian Bradfield for reporting this.

--- vm-reply.el
+++ vm-reply.el
@@ -44,7 +44,7 @@
 		     (let ((vm-summary-uninteresting-senders nil))
 				    (vm-summary-sprintf vm-in-reply-to-format
 							(car mp))))))
-	  (if (and vm-reply-subject-prefix
+	  (if (and this-subject vm-reply-subject-prefix 
 		   (not (string-match vm-reply-subject-prefix this-subject)))
               (setq this-subject (concat vm-reply-subject-prefix
                                          this-subject)))
