0f27a0e1a2cb1de973f02da3dc57766f2b7916e2
Robert Widhopf-Fenk <hack@robf.de>
2007-04-11 22:23:35 +0200

    Added snapshot tag and creation of "id" file.

--- release.sh
+++ release.sh
@@ -7,4 +7,8 @@ bzr export $dir
 cp configure $dir
 (cd lisp; make vm-revno.el)
 cp lisp/vm-revno.el $dir/lisp
+echo 'Version: $$Id: = '`bzr nick`-`bzr revno ` > $dir/id
 tar cvfz $dir.tgz $dir
+if [ -e $1 ]; then
+  ./$1 $dir.tgz;
+fi
