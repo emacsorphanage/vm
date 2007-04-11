6a88603449a394addcdde62cafe003be498f99c5
Robert Widhopf-Fenk <hack@robf.de>
2007-04-11 23:41:33 +0200

    Fixed generation of "id" file, vm-revno.el and tar ball.

--- release.sh
+++ release.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 make
-dir="release/`bzr nick`"
+rdir=`bzr nick`
+dir="release/$rdir"
 rm -rf $dir
 bzr export $dir
 cp configure $dir
-(cd lisp; make vm-revno.el)
+(cd lisp; rm vm-revno.el; make vm-revno.el)
 cp lisp/vm-revno.el $dir/lisp
-echo 'Version: $$Id: = '`bzr nick`-`bzr revno ` > $dir/id
-tar cvfz $dir.tgz $dir
-if [ -e $1 ]; then
+echo 'Version: $Id: '`bzr nick`-`bzr revno`'$' > $dir/id
+cd release
+tar cvfz $rdir.tgz $rdir
+cd ..
+if [ -n "$1" -a -e "$1" ]; then
   ./$1 $dir.tgz;
 fi
