7f33b936c3a93be987f112f9712f60c61f289ae8
Robert Widhopf-Fenk <hack@robf.de>
2007-05-09 23:09:54 +0200

    Disable plugins in BZR runs to avoid problems caused by them.

--- release.sh
+++ release.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 make
-rdir=`bzr nick`
+rdir=`bzr --no-plugins nick`
 dir="release/$rdir"
 rm -rf $dir
-bzr export $dir
+bzr --no-plugins export $dir
 cp configure $dir
 (cd lisp; rm vm-revno.el; make vm-revno.el)
 cp lisp/vm-revno.el $dir/lisp
-echo 'Version: $Id: '`bzr nick`-`bzr revno`'$' > $dir/id
+echo 'Version: $Id: '`bzr --no-plugins nick`-`bzr --no-plugins revno`'$' > $dir/id
 cd release
 tar cvfz $rdir.tgz $rdir
 cd ..
