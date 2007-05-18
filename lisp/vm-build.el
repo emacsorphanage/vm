f72d40d738c98cb4edfd5c24871136e0317f85bc
Robert Widhopf-Fenk <hack@robf.de>
2007-05-18 23:08:08 +0200

    Rewrote vm-build.el to handle also OTHERDIRS for EmacsW32.
    Also did some cleanup and linting of the code and added building of 
    autoloads for XEmacs (unused, as it does not work).

--- lisp/vm-build.el
+++ lisp/vm-build.el
@@ -1,13 +1,32 @@
 ;; Add the current dir to the load-path
 (setq load-path (cons default-directory load-path))
+;(setq debug-on-error t)
+
+(defun vm-fix-cygwin-path (path)
+  "If PATH does not exist, try the DOS path instead.
+This handles EmacsW32 path problems when building on cygwin."
+  (if (file-exists-p path)
+      path
+    (let ((dos-path (cond ((functionp 'mswindows-cygwin-to-win32-path)
+                           (mswindows-cygwin-to-win32-path path))
+                          ((string-match "^/cygdrive/\\([a-z]\\)" path)
+                           (replace-match (format "%s:" (match-string 1 path))
+                                          t t path)))))
+      (if (and dos-path (file-exists-p dos-path))
+          dos-path
+        path))))
 
 ;; Add additional dirs to the load-path
 (if (getenv "OTHERDIRS")
-    (let ((ps (delete "" (split-string (getenv "OTHERDIRS") "[:;]"))))
-      (while ps
-;        (message "adding user load-path: <%s>" (car ps))
-        (setq load-path (cons (car ps) load-path)
-              ps (cdr ps)))))
+    (let ((otherdirs (delete "" (split-string (getenv "OTHERDIRS") "[:;]")))
+          dir)
+      (while otherdirs
+        (setq dir (vm-fix-cygwin-path (car otherdirs)))
+        (message "adding user load-path: <%s>" dir)
+        (if (not (file-exists-p dir))
+            (error "Extra `load-path' directory %S does not exist!" dir))
+        (setq load-path (cons dir load-path)
+              otherdirs (cdr otherdirs)))))
 
 ;; Load byte compile 
 (require 'bytecomp)
@@ -22,40 +41,38 @@
 (require 'vm-vars)
 (require 'sendmail)
 
-(defun vm-fix-cygwin-path (path)
-  (cond ((functionp 'mswindows-cygwin-to-win32-path)
-         (mswindows-cygwin-to-win32-path path))
-        ((string-match "^/cygdrive/\\([a-z]\\)" path)
-         (replace-match (format "%s:" (match-string 1 path)) t t path))
-        (t
-         path)))
-
-(defun vm-built-autoloads ()
-  (setq file (car command-line-args-left)
-	dir (car (cdr command-line-args-left))
-        debug-on-error t)
-  (if (not (file-exists-p dir))
-      ;; to handle emacsW32 path problems when building on cygwin
-      (setq file (vm-fix-cygwin-path file)
-            dir (vm-fix-cygwin-path dir)))
-  (if (not (file-exists-p dir))
-      (error "Built %S directory does not exist!" dir))
-  (message "Building autoloads file %S\nin directory %S." file dir)
-  (load-library "autoload")
-  (set-buffer (find-file-noselect file))
-  (erase-buffer)
-  (setq generated-autoload-file file)
-  (setq make-backup-files nil)
-  (if (featurep 'xemacs)
-      (error "This is only for GNU Emacs.")
-    ;; GNU Emacs 21 wants some content, but 22 does not like it ...
-    (insert ";;; vm-autoloads.el --- automatically extracted autoloads\n")
-    (insert ";;\n")
-    (insert ";;; Code:\n")
-    (if (>= emacs-major-version 21)
-	(update-autoloads-from-directories dir)
-      (if (>= emacs-major-version 22)
-	  (update-directory-autoloads dir)
-	(error "Do not know how to generate autoloads")))))
+(defun vm-built-autoloads (&optional autoloads-file source-dir)
+  (let ((autoloads-file (or autoloads-file
+                            (vm-fix-cygwin-path (car command-line-args-left))))
+	(source-dir (or source-dir
+                        (vm-fix-cygwin-path (car (cdr command-line-args-left)))))
+        (debug-on-error t)
+        (enable-local-eval nil))
+    (if (not (file-exists-p source-dir))
+        (error "Built directory %S does not exist!" source-dir))
+    (message "Building autoloads file %S\nin directory %S." autoloads-file source-dir)
+    (load-library "autoload")
+    (set-buffer (find-file-noselect autoloads-file))
+    (erase-buffer)
+    (setq generated-autoload-file autoloads-file)
+    (setq autoload-package-name "vm")
+    (setq make-backup-files nil)
+    (if (featurep 'xemacs)
+        (progn
+          (update-autoloads-from-directory source-dir)
+          (fixup-autoload-buffer (concat (if autoload-package-name
+                                             autoload-package-name
+                                           (file-name-nondirectory defdir))
+                                         "-autoloads"))
+          (save-some-buffers t))
+      ;; GNU Emacs 21 wants some content, but 22 does not like it ...
+      (insert ";;; vm-autoloads.el --- automatically extracted autoloads\n")
+      (insert ";;\n")
+      (insert ";;; Code:\n")
+      (if (>= emacs-major-version 21)
+          (update-autoloads-from-directories source-dir)
+        (if (>= emacs-major-version 22)
+            (update-directory-autoloads source-dir)
+          (error "Do not know how to generate autoloads"))))))
 
 (provide 'vm-build)
