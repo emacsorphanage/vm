499fba069c649b8f9e0c6e7467efb6a0e83b3eeb
Robert Widhopf-Fenk <hack@robf.de>
2007-05-19 23:27:19 +0200

    Use a var for bzr with right command line args.

--- release.sh
+++ release.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 make
-rdir=`bzr --no-plugins nick`
+bzr=bzr --no-plugins --no-aliases
+rdir=`$bzr nick`
 dir="release/$rdir"
 rm -rf $dir
-bzr --no-plugins export $dir
+$bzr export $dir
 cp configure $dir
 (cd lisp; rm vm-revno.el; make vm-revno.el)
 cp lisp/vm-revno.el $dir/lisp
-echo 'Version: $Id: '`bzr --no-plugins nick`-`bzr --no-plugins revno`'$' > $dir/id
+echo 'Version: $Id: '`$bzr nick`-`$bzr revno`'$' > $dir/id
 cd release
 tar cvfz $rdir.tgz $rdir
 cd ..
