1791ca42a9d360169706b4ac95ee92509db272a9
Robert Widhopf-Fenk <hack@robf.de>
2007-05-29 01:26:15 +0200

    Include revno in release tarball.

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ bzr diff || exit 1
 
 make
 bzr="bzr --no-plugins --no-aliases"
-rdir=`$bzr nick`
+rdir=`$bzr nick`-`$bzr revno`
 dir="release/$rdir"
 rm -rf $dir
 mkdir -p release
@@ -12,7 +12,7 @@ $bzr export $dir
 cp configure $dir
 (cd lisp; rm vm-revno.el; make vm-revno.el)
 cp lisp/vm-revno.el $dir/lisp
-echo 'Version: $Id: '`$bzr nick`-`$bzr revno`'$' > $dir/id
+echo 'Version: $Id: '$rdir'$' > $dir/id
 cd release
 tar cvfz $rdir.tgz $rdir
 cd ..
