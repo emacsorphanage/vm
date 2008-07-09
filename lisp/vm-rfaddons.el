ba264d7aed05cb7034e1df30bc107046a1c1d0db
Robert Widhopf-Fenk <hack@robf.de>
2008-07-09 21:17:27 +0200

    Force before-change-functions and after-change-functions to be local.
    The t arg to add-hook does not work for them unfortunately.

--- lisp/vm-rfaddons.el
+++ lisp/vm-rfaddons.el
@@ -1800,6 +1800,9 @@ and add an \"%0UA\" to your `vm-summary-format'."
 (defun vm-mail-mode-install-open-line ()
   "Install the open-line hooks for `vm-mail-mode'.
 Add this to `vm-mail-mode-hook'."
+  ;; these are not local even when using add-hook, so we make them local
+  (make-local-hook 'before-change-functions)
+  (make-local-hook 'after-change-functions)
   (add-hook 'before-change-functions 'vm-mail-mode-open-line nil t)
   (add-hook 'after-change-functions 'vm-mail-mode-open-line nil t))
 
