3b492dfdcd8b77651bf33d179b277345d3c0cc5c
Uday S Reddy <u.s.reddy@cs.bham.ac.uk>
2009-12-19 10:04:21 +0000

    This fixes a jumping cursor problem in the summary buffer reported in the Launchpad bug report #497571.  The problem is apparently that save-excursion only records the position of the cursor in the current buffer.  So, it doesn't make sense to do save-excursion -> set-buffer. One should do set-buffer -> save-excursion.  Even v7.19 has this problem in many places!

--- lisp/vm-summary.el
+++ lisp/vm-summary.el
@@ -240,9 +240,9 @@ mandatory."
 	  (let ((buffer-read-only nil)
 		(selected nil)
 		(modified (buffer-modified-p)))
-	    (goto-char (vm-su-start-of m))
 	    (unwind-protect
 		(save-excursion
+		  (goto-char (vm-su-start-of m))
 		  (setq selected (looking-at "[+-]>"))
 		  ;; We do a little dance to update the text in
 		  ;; order to make the markers in the text do
