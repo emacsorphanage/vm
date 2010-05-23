5fe829b9fa179691769e4c70fe94ba372c1a7eff
Ulrich Müller <ulm@gentoo.org>
2010-05-23 17:41:56 +0200

    Fix header comment.

--- lisp/vm-reply.el
+++ lisp/vm-reply.el
@@ -19,8 +19,9 @@
 ;; with this program; if not, write to the Free Software Foundation, Inc.,
 ;; 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-;;; Interface:
+;;; Commentary:
 
+;; Interface:
 ;; Interactive commands:
 ;;
 ;; vm-yank-message: (message) -> unit
